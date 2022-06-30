4bb9c144414ad86e9a31607de012a994aa6995af
KANGDONGHOON0413 <kmerits@gmail.com>
2022-06-30 20:26:22 +0900

    초기 커밋.

---
+++ day17_01/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿
+namespace day17_01
+{
+    partial class kang01
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // kang01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "kang01";
+            this.Text = "kang";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.kang01_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.kang01_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ day17_01/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day17_01
+{
+    public partial class kang01 : Form
+    {
+        public kang01()
+        {
+            InitializeComponent();
+        }
+
+        //keydown event는 키눌림을 위한 이벤트로 keys열거형으로 키 구분
+        // 문자키는 모두 대문자로 입력된다. 즉, 대소문자 구분을 못한다.
+        private void kang01_KeyDown(object sender, KeyEventArgs e)
+        {
+            //if(e.KeyCode == Keys.A && e.Shift && e.Control)
+            //{
+            //    MessageBox.Show("A + ctrl + shift");
+            //}
+
+            /*
+            switch (e.KeyCode)
+            {
+                case Keys.A: MessageBox.Show("A checked"); break;
+                case Keys.B: MessageBox.Show("B checked"); break;
+                case Keys.C: MessageBox.Show("C checked"); break;
+            }
+            */
+
+            if(e.Modifiers == (Keys.Alt | Keys.Shift))
+            {
+                MessageBox.Show("alt + shift");
+            }
+
+
+
+            //밑의 두방식은 결과가 같다.
+
+            /*  Keycode를 이용한 방식
+            if(e.KeyCode == Keys.A && e.Modifiers == (Keys.Control | Keys.Shift))
+            {
+                MessageBox.Show("A + shift + ctrl");
+            }
+            */
+
+            //KeyData를 이용한 방식
+            if(e.KeyData == (Keys.A | Keys.Control | Keys.Shift))
+            {
+                MessageBox.Show("A + shift + ctrl");
+            }
+        }
+
+        
+        private void kang01_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            MessageBox.Show("keyPress");
+        }
+    }
+}
